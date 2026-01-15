f2073949c95ca48caa29c89f89cc3c7833d4337d
Nitish Kumar <nitishkumaryadav105@gmail.com>
2026-01-15 17:05:42 +0530

    update welcome file

--- src/main/java/com/jenkins/dev/controller/RootController.java
+++ src/main/java/com/jenkins/dev/controller/RootController.java
@@ -1,14 +1,20 @@
 package com.jenkins.dev.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class RootController {
 
     @GetMapping("/")
-    public String greeting(){
-         return "Hello from Nitish Kumar through jenkins pipeline ";
+    public String greeting(Model model){
+        // return "Hello from Nitish Kumar through jenkins pipeline ";
+        model.addAttribute("deployedBy", "Nitish Kumar");
+        model.addAttribute("deploymentMethod", "Jenkins Pipeline");
+        model.addAttribute("deploymentTime", new java.util.Date());
+        model.addAttribute("status", "Active and Running");
+        return "index"; // This will look for src/main/resources/templates/index.html
     }
 }
